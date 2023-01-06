4bcb1b9c88a50682c0b7680057b0526e0d03aa7e
Emran Khan <63871014+xemran@users.noreply.github.com>
2023-01-07 00:04:25 +0600

    Update randomPractice.js

--- randomPractice.js
+++ randomPractice.js
@@ -11,4 +11,42 @@
 
 </script> -->
 
+<!-- <!-- Practicing javaScript Objects and methods as well as this  -->
+=========================================================================
+<!-- <p id='id'></p>
+sss
+
+<script>
+  let element = document.getElementById('id');
+  const hum = {
+    name : "Emran",
+    skill: "Fighter",
+    programmingLanguage: "javaScript",
+    fun : function() {
+      return this.name+" "+this.skill;
+    }
+  
+  }
+   
+  
+  element.innerHTML = hum.fun();
+  
+</script>  -->
+
+
+<!--  practicing js string methods -->
+===========================================
+<!-- <p id='id'></p>
+
+
+<script>
+
+  sot = function(){
+        let element = document.getElementById('id');
+        aa = "this is the osthir string"
+    element.innerHTML = aa.slice(0,aa.length);
+  }
+  sot()
+  
+</script>  -->
 
