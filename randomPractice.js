2c30702127edf8b98b3c958ca372fbb6bd3fc7d6
Emran Khan <63871014+xemran@users.noreply.github.com>
2023-01-06 17:13:56 +0600

    Create randomPractice.js

---
+++ randomPractice.js
@@ -0,0 +1,14 @@
+// The Nullish Coalescing Operator
+=====================================
+
+<p id='p'></p> // cat
+
+<script>
+ y = 'cat'
+ x = null
+  x??=y;
+  document.getElementById('p').innerHTML=x
+
+</script> -->
+
+
